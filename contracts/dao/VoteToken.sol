7572020ab4be02151705dfe4f722e4ac6d1356ec
Solo <60989790+solo7777@users.noreply.github.com>
2025-07-31 20:47:47 +0200

    Update VoteToken.sol

--- contracts/dao/VoteToken.sol
+++ contracts/dao/VoteToken.sol
@@ -1,43 +1,31 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.28;
+pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 contract VoteToken is ERC20Votes {
-    constructor()
-        ERC20("HackDAO Vote Token", "HVT")
-        ERC20Permit("HackDAO Vote Token")
+    constructor(string memory name, string memory symbol)
+        ERC20(name, symbol)
+        ERC20Permit(name)
+        EIP712(name, "1")
     {
-        _mint(msg.sender, 1_000_000 * 10 ** decimals());
+        _mint(msg.sender, 1000000 * 10 ** decimals());
     }
 
-    function _afterTokenTransfer(address from, address to, uint256 amount)
-        internal override(ERC20Votes)
+    function _update(address from, address to, uint256 value)
+        internal
+        virtual
+        override(ERC20Votes)
     {
-        super._afterTokenTransfer(from, to, amount);
-    }
-
-    function _mint(address to, uint256 amount)
-        internal override(ERC20Votes)
-    {
-        super._mint(to, amount);
-    }
-
-    function _burn(address account, uint256 amount)
-        internal override(ERC20Votes)
-    {
-        super._burn(account, amount);
-    }
-
-    function _update(address from, address to, uint256 amount)
-        internal override(ERC20Votes)
-    {
-        super._update(from, to, amount);
+        super._update(from, to, value);
     }
 
     function nonces(address owner)
-        public view override(ERC20Permit)
+        public
+        view
+        virtual
+        override(Nonces)
         returns (uint256)
     {
         return super.nonces(owner);
